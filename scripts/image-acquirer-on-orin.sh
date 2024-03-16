91025a8c412dc236a52da9dd57f0b558cdbb40f7
Koo Kin Wai <kkoo@redhat.com>
2024-03-16 17:09:28 +0800

    Run the image-acquirer on the Orin

---
+++ scripts/image-acquirer-on-orin.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+IMAGE=ghcr.io/kwkoo/image-acquirer
+MQTT_SERVER=mosquitto-demo.apps.replace.me
+
+podman run \
+  --name image-acquirer \
+  --rm \
+  -it \
+  -p 8080:8080 \
+  --runtime /usr/bin/nvidia-container-runtime \
+  --group-add keep-groups \
+  --security-opt label=disable \
+  --env NVIDIA_DRIVER_CAPABILITIES=all \
+  --env CAMERA=/videos/video.mp4 \
+  --env MQTT_SERVER=$MQTT_SERVER \
+  --env MQTT_PORT=80 \
+  --env MQTT_TRANSPORT=websockets \
+  --env MQTT_TOPIC=alerts \
+  --env MODEL=NCS_YOLOv8-20Epochs.pt \
+  --env CONFIDENCE=0.5 \
+  -v ./Gun_Video.mp4:/videos/video.mp4 \
+  $IMAGE
