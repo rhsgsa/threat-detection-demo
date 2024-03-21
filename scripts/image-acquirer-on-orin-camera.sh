fa103c5fd8592e4c2cb74f138f23eeb10513f849
Koo Kin Wai <kin.wai.koo@gmail.com>
2024-03-21 18:09:26 +0800

    Script to run with webcam as video source

--- scripts/image-acquirer-on-orin-camera.sh
+++ scripts/image-acquirer-on-orin-camera.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This script runs the image-acquirer using a webcam as a source
+# Ensure that /dev/video0 exists before running this script
+# Don't forget to set MQTT_SERVER to point to your actual server
+
 IMAGE=ghcr.io/kwkoo/image-acquirer
 MQTT_SERVER=mosquitto-demo.apps.replace.me
 
@@ -8,17 +12,17 @@ podman run \
   --rm \
   -it \
   -p 8080:8080 \
+  --device /dev/video0:/dev/video0 \
   --runtime /usr/bin/nvidia-container-runtime \
   --group-add keep-groups \
   --security-opt label=disable \
   --env NVIDIA_DRIVER_CAPABILITIES=all \
   --env NVIDIA_VISIBLE_DEVICES=0 \
-  --env CAMERA=/videos/video.mp4 \
+  --env CAMERA=/dev/video0 \
   --env MQTT_SERVER=$MQTT_SERVER \
   --env MQTT_PORT=80 \
   --env MQTT_TRANSPORT=websockets \
   --env MQTT_TOPIC=alerts \
   --env MODEL=NCS_YOLOv8-20Epochs.pt \
   --env CONFIDENCE=0.5 \
-  -v ./Gun_Video.mp4:/videos/video.mp4 \
   $IMAGE
---
+++ scripts/image-acquirer-on-orin-video.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# This script runs the image-acquirer using a video file as a source
+# Don't forget to set MQTT_SERVER to point to your actual server
+
+IMAGE=ghcr.io/kwkoo/image-acquirer
+MQTT_SERVER=mosquitto-demo.apps.replace.me
+
+podman run \
+  --name image-acquirer \
+  --rm \
+  -it \
+  -p 8080:8080 \
+  --runtime /usr/bin/nvidia-container-runtime \
+  --group-add keep-groups \
+  --security-opt label=disable \
+  --env NVIDIA_DRIVER_CAPABILITIES=all \
+  --env NVIDIA_VISIBLE_DEVICES=0 \
+  --env CAMERA=/videos/video.mp4 \
+  --env MQTT_SERVER=$MQTT_SERVER \
+  --env MQTT_PORT=80 \
+  --env MQTT_TRANSPORT=websockets \
+  --env MQTT_TOPIC=alerts \
+  --env MODEL=NCS_YOLOv8-20Epochs.pt \
+  --env CONFIDENCE=0.5 \
+  -v ./Gun_Video.mp4:/videos/video.mp4 \
+  $IMAGE
