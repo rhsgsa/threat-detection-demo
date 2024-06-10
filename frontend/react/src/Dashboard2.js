c4ee97cb20631e2180fd678cc601870bbc161968
condescendante <condescendanteee@gmail.com>
2024-06-10 16:13:14 +0800

    Change default button state to active and add get api call

--- frontend/react/src/Dashboard2.js
+++ frontend/react/src/Dashboard2.js
@@ -242,6 +242,22 @@ function Promptlist () {
     return <Badge variant='solid' colorScheme={ colour } fontSize='0.8em'>{ threat }</Badge>
   }
 
+    //GET Display Data on Button Click
+    function SubmitHandler () {
+      useEffect(() => {
+        const callAxios = async () => {
+          await axios
+            .get(baseurl + '/api/resumeevents')
+            .then(response => {
+              console.log('SUCCESS', response);
+            })
+            .catch(error => {
+              console.log(error);
+            });
+        };
+        callAxios();
+      }, []);
+    }
 
 function Dashboard2 () {
     const [ isLoaded, setIsLoaded ] = useState(true);
@@ -298,11 +314,11 @@ function Dashboard2 () {
         });
 
         evtSource.addEventListener("pause_events", event => {
-          setShowButton(false);
+          setShowButton(true);
         });
         
         evtSource.addEventListener("resume_events", event => {
-          setShowButton(true);
+          setShowButton(false);
         });
     }, []);
     
@@ -351,7 +367,15 @@ function Dashboard2 () {
                           >
                             <Photo annotatedImage={annotatedImage} rawImage={rawImage}/>
                           </Skeleton>
-                          <Button colorScheme='blue' isDisabled={showButton}> Resume Stream </Button>
+                          <Button 
+                            colorScheme='blue' 
+                            isActive={showButton}
+                            onClick={() => {
+                              SubmitHandler();
+                            }}
+                          > 
+                            Resume Stream 
+                          </Button>
                         </Stack>
                     </CardBody>
                 </Card>
